f0f233b5230e7845f2d047e0122445c5241cd403
andreybozhkov <bozhkov.andrey@gmail.com>
2019-05-01 17:30:02 +0300

    Added initial database connection

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const app = require('express')();
 const port = 3000;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => res.json({ testKey: 'testValue' }));
+
+mongoose.connect('mongodb://localhost:27017/cmr-app-db', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.once('open', err => {
+    if (err) throw err;
+
+    console.log('Connection opened!');
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
